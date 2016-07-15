bca10aa182bfa04aa5d7d94d880ad99880c977f2
Alli Barnes <alli.cummings@gmail.com>
2016-07-15 15:07:29 -0400

    add navbar work, logo options

--- js/init.js
+++ js/init.js
@@ -1,8 +1,12 @@
 (function($){
+  console.log("jquery loaded");
   $(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $(".dropdown-button").dropdown(
+      { hover: false }
+    ); // activates drodown option in NAV menu
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
